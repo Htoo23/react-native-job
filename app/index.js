e854ca80dca175b2460fe486cf5c59d85bb928ff
Htoo Aung Lin <htooaunglin@yomabank.com>
2023-10-29 10:20:16 +0630

    ScreenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {View,SafeAreaView,ScrollSaf} from 'react-native';
+import {View,SafeAreaView,ScrollSaf,ScrollView} from 'react-native';
 import { Stack,useRouter } from 'expo-router';
 import { COLORS,icons,images,SIZES } from '../constants';
 import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from '../components';
@@ -10,7 +10,24 @@ const Home=()=>{
         <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
 
             <Stack.Screen options={{headerStyle:{backgroundColor:COLORS.lightWhite},
+            headerShadowVisible:false,
+            headerLeft:()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+            ),
+            headerRight:()=>(
+                <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+            ),
+            headerTitle:""
         }}/>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={{flex:1,padding:SIZES.medium}}>
+                <Welcome/>
+                <Popularjobs/>
+                <Nearbyjobs/>
+
+            </View>
+
+        </ScrollView>
         </SafeAreaView>
     )
 }
